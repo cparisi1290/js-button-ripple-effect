7988565109c1a982802946d9b397d35c57070209
Cassandra <cparisi1290@gmail.com>
2021-11-07 09:59:49 -0500

    create span in button and give it properties of the x and y of inside the button. change arrow function to regular function to append new element

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const buttons = document.querySelectorAll('.ripple');
 
 buttons.forEach((button) => {
-  button.addEventListener('click', (e) => {
+  button.addEventListener('click', function (e) {
     const x = e.clientX;
     const y = e.clientY;
     // console.log(x, y);
@@ -13,5 +13,12 @@ buttons.forEach((button) => {
     const xInside = x - buttonLeft;
     const yInside = y - buttonTop;
     // console.log(xInside, yInside);
+
+    const circle = document.createElement('span');
+    circle.classList.add('circle');
+    circle.style.top = yInside + 'px';
+    circle.style.left = xInside + 'px';
+
+    this.appendChild(circle);
   });
 });
