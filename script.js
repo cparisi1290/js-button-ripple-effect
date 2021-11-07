88dbd44e6e6cd2e5ec9a3f9730bf3ac8b0183bda
Cassandra <cparisi1290@gmail.com>
2021-11-07 09:56:41 -0500

    loop over each button, find point where button is clicked inside of button

---
+++ script.js
@@ -0,0 +1,17 @@
+const buttons = document.querySelectorAll('.ripple');
+
+buttons.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    const x = e.clientX;
+    const y = e.clientY;
+    // console.log(x, y);
+
+    const buttonTop = e.target.offsetTop;
+    const buttonLeft = e.target.offsetLeft;
+    // console.log(buttonTop, buttonLeft);
+
+    const xInside = x - buttonLeft;
+    const yInside = y - buttonTop;
+    // console.log(xInside, yInside);
+  });
+});
